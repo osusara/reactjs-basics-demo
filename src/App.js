435a1d6838cd831100c74a10954d2c866699686d
Osusara Kammalawatta <osusarak96@gmail.com>
2020-12-03 13:34:15 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,56 +1,17 @@
 import React from 'react';
 import './App.css';
 
-import Value from "./components/Value";
-import Buttons from "./components/Buttons";
+
 import Lifecycle from "./components/Lifecycle";
+import Basics from "./components/basics/Basics";
 
-// Buttons component
-// class Buttons extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={() => this.props.updateCountValue(1)}>+1</button>
-//         <button onClick={() => this.props.updateCountValue(-1)}>-1</button>
-//       </div>
-//     );
-//   }
-// }
-
-// Value componenet
-// class Value extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <h1>{this.props.countValue}</h1>
-//       </div>
-//     );
-//   }
-// }
-
-// App component
 class App extends React.Component {
   constructor(props) {
     super(props);
-
-    // state
     this.state = {
-      count: 0,
-      lifecycle: true
+      lifecycle: true,
     };
-
-    // Bind in constructor
-    // this.updateCount = this.updateCount.bind(this);
   }
-
-  updateCount(value) {
-    this.setState({ count: this.state.count + value });
-  }
-
-  // Does not need to bind arrow functions
-  // updateCount = (value) => {
-  //   this.setState({ count: this.state.count + value });
-  // }
 
   toggleComponent() {
     this.setState({ lifecycle: !this.state.lifecycle });
@@ -59,18 +20,19 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        {/* Components, Props, State demo */}
-        <Value countValue={this.state.count} />
-        <Buttons updateCountValue={this.updateCount.bind(this)} />
+        <div id="component-props-state-demo">
+          <Basics />
+        </div>
 
         <hr />
-        
-        {/* Lifecycle methods demo */}
-        {this.state.lifecycle ? <Lifecycle /> : null}
-        <br />
-        <button onClick={this.toggleComponent.bind(this)}>
-          (Un)mount Lifecycle Component
-        </button>
+
+        <div id="lifecycle-method-demo">
+          {this.state.lifecycle ? <Lifecycle /> : null}
+          <br />
+          <button onClick={this.toggleComponent.bind(this)}>
+            (Un)mount Lifecycle Component
+          </button>
+        </div>
       </div>
     );
   }
---
+++ src/components/basics/Basics.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+
+import Value from "./Value";
+import Buttons from "./Buttons";
+
+// Buttons component
+// class Buttons extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={() => this.props.updateCountValue(1)}>+1</button>
+//         <button onClick={() => this.props.updateCountValue(-1)}>-1</button>
+//       </div>
+//     );
+//   }
+// }
+
+// Value componenet
+// class Value extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.props.countValue}</h1>
+//       </div>
+//     );
+//   }
+// }
+
+class Basics extends Component {
+  constructor(props) {
+    super(props);
+
+    // state
+    this.state = {
+      count: 0,
+    };
+
+    // Bind in constructor
+    // this.updateCount = this.updateCount.bind(this);
+  }
+
+  updateCount(value) {
+    this.setState({ count: this.state.count + value });
+  }
+
+  // Does not need to bind arrow functions
+  // updateCount = (value) => {
+  //   this.setState({ count: this.state.count + value });
+  // }
+
+  render() {
+    return (
+      <div>
+        <Value countValue={this.state.count} />
+        <Buttons updateCountValue={this.updateCount.bind(this)} />
+      </div>
+    );
+  }
+}
+
+export default  Basics;
